36cee5417141422b8edbf7e60f3cd59a88807ba6
nowakj-catapult <josephnowak119@gmail.com>
2025-06-25 10:33:39 -0500

    final grass not in main before importing to main

--- grass_move_by.py
+++ grass_move_by.py
@@ -3,54 +3,6 @@ import sys
 import random
 import time
 
-
-def main():
-    pygame.init()
-
-
-
-    screen = pygame.display.set_mode((1200, 620))
-    grass1 = Grass(screen, 1200, 580)
-    grass2 = Grass(screen, 900, 580)
-    grass3 = Grass(screen, 600, 580)
-    grass4 = Grass(screen, 300, 580)
-    grass5 = Grass(screen, 000, 580)
-    clock = pygame.time.Clock()
-    for i in range(6):
-        x = i* grass_for_flappy.png
-        y = 580
-        g = Grass(x,y)
-        grass_list.append(g)
-
-    # TODO 05: Change the window size, make sure your circle code still works.
-    while True:
-        screen.fill((255, 255, 255))
-        clock.tick(60)
-
-        for g in grass_list:
-            g.move()
-
-        if g in grass_list[0].off_screen :
-            del grass_list[0]
-            x = grass_list[-1].x _ grass_img.get_width()
-            y = 580
-            new_grass = Grass (x,y)
-            grass_list.append(new_grass)
-
-
-
-
-
-
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        clock = pygame.time.Clock()
-        font = pygame.font.SysFont('Arial', 40)
-        pygame.display.update()
-
-
 class Grass:
     def __init__(self, screen, x, y):
         """ Creates a Raindrop sprite that travels down at a random speed. """
@@ -65,30 +17,70 @@ class Grass:
         self.y = y
         self.speed = 5
         self.grass_list = []
+        grass_list = self.grass_list
 
 
     def move(self):
 
         self.x = self.x - self.speed
 
-    def off_screen(self):
-        """ Returns true if the Raindrop y value is not shown on the screen, otherwise false. """
-        # Note: this will be used for testing, but not used in the final version of the code for the sake of simplicity.
-        # TODO 13: Return  True  if the  y  position of this Raindrop is greater than 800.
-        self.x < -328
 
     def off_screen(self):
         """ Returns true if the Raindrop y value is not shown on the screen, otherwise false. """
-        # Note: this will be used for testing, but not used in the final version of the code for the sake of simplicity.
-        # TODO 13: Return  True  if the  y  position of this Raindrop is greater than 800.
-        image_length = 394
-        return self.x < 0 + image_length
+        image_length = 384
+        return self.x < 0 - image_length
 
     def draw(self):
         """ Draws this sprite onto the screen. """
         # TODO 9: Draw a vertical line that is 5 pixels long, 2 pixels thick,
         #      from the current position of this Raindrop (use either a black or blue color).
         self.screen.blit(self.image1, (self.x ,580))
+
+def main():
+    pygame.init()
+
+
+
+    screen = pygame.display.set_mode((1200, 620))
+    grass1 = Grass(screen, 1200, 580)
+    grass2 = Grass(screen, 900, 580)
+    grass3 = Grass(screen, 600, 580)
+    grass4 = Grass(screen, 300, 580)
+    grass5 = Grass(screen, 000, 580)
+    clock = pygame.time.Clock()
+    grass_list = []
+    for i in range(6):
+        x = i* 394
+        y = 580
+        g = Grass(screen, x, 580)
+        grass_list.append(g)
+
+    # TODO 05: Change the window size, make sure your circle code still works.
+    while True:
+        screen.fill((255, 255, 255))
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        clock = pygame.time.Clock()
+
+        for g in grass_list:
+            g.move()
+            g.draw()
+
+        if grass_list[0].off_screen() :
+            del grass_list[0]
+            x = grass_list[-1].x + 328
+            y = 580
+            new_grass = Grass (screen, x,y)
+            grass_list.append(new_grass)
+
+
+
+        font = pygame.font.SysFont('Arial', 40)
+        pygame.display.update()
+
 
 
 
--- ground_and_background.py
+++ ground_and_background.py
@@ -3,17 +3,110 @@ import sys
 import random
 import time
 
-IMAGE_SIZE_X = 32
-IMAGE_SIZE_Y = 18
 
-screen = pygame.display.set_mode((1200, 620))
-#
-image1 = pygame.image.load("grass.png")
-    #
-image1 = pygame.transform.scale(image1, (IMAGE_SIZE_X, IMAGE_SIZE_Y))
-# TODO 2: Draw (blit) the image onto the screen at position (0, 0)
+WIDTH, HEIGHT = 1200, 620
+BIRD_SIZE = 60
+gravity = 0.5
+pipe_gap = 150
+pipe_speed = 5
 
-while True:
-    screen.fill((250, 250, 0))
-    screen.blit(image1, (0, 0))
-    screen.blit(image1, (0,620- IMAGE_SIZE_Y))
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (135, 206, 250)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+BACKROUND = pygame.image.load("rose_background.png")
+BACKROUND = pygame.transform.scale(BACKROUND, (1200, 620))
+
+class Bird:
+    def __init__(self, screen, x, y, bird_image1):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(bird_image1)
+        self.image = pygame.transform.scale(self.image, (BIRD_SIZE, BIRD_SIZE))
+
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
+    def get_rect(self):
+        return pygame.Rect(self.x, self.y, BIRD_SIZE, BIRD_SIZE)
+
+def draw_pipes(screen, x, height):
+    bottom_pipe = pygame.Rect(x, height + pipe_gap, 60, HEIGHT)
+    top_pipe = pygame.Rect(x, 0, 60, height)
+    pygame.draw.rect(screen, RED, bottom_pipe)
+    pygame.draw.rect(screen, RED, top_pipe)
+    return top_pipe, bottom_pipe
+
+def check_collision(bird, pipes):
+    bird_rect = bird.get_rect()
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+    if bird_rect.top <= 0 or bird_rect.bottom >= HEIGHT:
+        return False
+    return True
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Flappy Bird")
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont('Arial', 40)
+
+    bird = Bird(screen, 310, HEIGHT // 2, "pink_bird.png")
+    bird_movement = 0
+    game_active = True
+    score = 0
+
+    pipe_x = WIDTH
+    pipe_height = random.randint(150, 450)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if game_active:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    bird_movement = -8
+            else:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    bird.y = HEIGHT // 2
+                    pipe_x = WIDTH
+                    pipe_height = random.randint(150, 450)
+                    score = 0
+                    bird_movement = 0
+                    game_active = True
+
+        screen.blit(BACKROUND, (0, 0))
+
+        if game_active:
+            bird_movement += gravity
+            bird.y += int(bird_movement)
+
+            pipe_x -= pipe_speed
+            if pipe_x + 60 < 0:
+                pipe_x = WIDTH
+                pipe_height = random.randint(150, 450)
+                score += 1
+
+            pipes = draw_pipes(screen, pipe_x, pipe_height)
+            bird.draw()
+            game_active = check_collision(bird, pipes)
+
+            score_text = font.render(f"Score: {score}", True, BLACK)
+            screen.blit(score_text, (10, 10))
+        else:
+            game_over_text = font.render("Game Over!", True, BLACK)
+            screen.blit(game_over_text, (WIDTH // 2 - 100, HEIGHT // 2 - 40))
+            restart_text = font.render("Press SPACE to Restart", True, BLACK)
+            screen.blit(restart_text, (WIDTH // 2 - 150, HEIGHT // 2 + 10))
+            bird.draw()
+
+        pygame.display.update()
+        clock.tick(60)
+
+main()
